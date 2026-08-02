0a80738cd6af82d0a52f295fc786db502ef62e70
rafajob <56689600+rafajob@users.noreply.github.com>
2026-08-02 12:06:47 -0300

    Add local configuration example

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'alimentacao',
+    'username' => 'alimentacao_app',
+    'password' => 'troque-esta-senha',
+];
